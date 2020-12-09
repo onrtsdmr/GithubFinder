bfbbd241ce90532707b927ebaca1c76ebff7a943
onrtsdmr <onurtaademir@gmail.com>
2020-12-09 13:24:52 +0300

    Changed favicon.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,27 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {LayoutModule} from "./layout/layout.module";
+import {DashboardModule} from "./components/dashboard/dashboard.module";
+import {NotFoundModule} from "./components/not-found/not-found.module";
+import {HomeModule} from "./components/home/home.module";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    LayoutModule,
+    DashboardModule,
+    NotFoundModule,
+    HomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
